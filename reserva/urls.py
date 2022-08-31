2556a492d4ebdf937d611b7541d8005a32cd9cfb
Leonardo Morais <leonardo.morais7778@gmail.com>
2022-08-30 22:06:45 -0300

    Colocando imagens do DB no index

--- reserva/urls.py
+++ reserva/urls.py
@@ -1,3 +1,5 @@
+from django.conf.urls.static import static
+from django.conf import settings
 from django.urls import path
 from . import views
 
@@ -7,4 +9,4 @@ urlpatterns =[
     path('<int:espaco_id>', views.descricao, name='descricao'),
     #Reservas
     path('registro/', views.registro, name='registro'),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
