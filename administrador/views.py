619719dc40d99f8a61baa557417379d8faf62d4f
Leonardo Morais <leonardo.morais7778@gmail.com>
2022-10-19 22:03:55 -0300

    Tentando modificar o nivel de acesso

--- administrador/views.py
+++ administrador/views.py
@@ -74,9 +74,14 @@ def editar_adm(request,usuario_id):
     if request.method == 'POST':
         nome = request.POST['nome_usuario']
         email = request.POST['email']
+        # tipo = request.POST['nivel']
         usuario.username = nome
         usuario.email = email
         usuario.save()
+        # user_id = User.objects.get(email = email)
+        # user_n = get_object_or_404(User, pk= user_id.id)
+        # nivel = NivelUsuario.objects.create(usuario = user_n, status= tipo)
+        # nivel.save()
         return redirect('gerenciar_usuario')
     usuario = get_object_or_404(User, pk=usuario_id)
     return render(request, 'editar_adm.html', {'usuario' : usuario})
